c28cd4f147fdc209ec3a503eb7df14982c6849a6
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-10-15 19:19:57 +0530

    Form, post and their components added

---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const Posts = () => {
+  return (
+    <div>
+      <h1>POST</h1>
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
